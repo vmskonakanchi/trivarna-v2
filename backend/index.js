3bf0b486998001d8baa5c654f593a7dfb06aba91
Gowtham <gowtham@gmail.com>
2022-12-13 00:21:48 +0530

    added express json middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import config from './config';
+import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
 import cors from "cors";
 
 const port = config.PORT;
@@ -8,17 +8,17 @@ const port = config.PORT;
 const app = express();
 
 if (!config.PRODUCTION) {
-    app.use(cors())
+  app.use(cors());
 }
 
+app.use(express.json());
 
 mongoose.connect(config.DATABASE_URL, (err) => {
-    if (err) {
-        console.log("error", err)
-    } else {
-        app.listen(port, () => {
-            console.log(`Server listenting on port ${port}`)
-        })
-    }
-})
-
+  if (err) {
+    console.log("error", err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Server listenting on port ${port}`);
+    });
+  }
+});
