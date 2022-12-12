7e43cbb0d9c475c04eaf5e6a6400d4898e4c3f98
Vamsi <vamsi@gmail.com>
2022-12-13 00:03:17 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import cors from "cors";
+
+const port = config.PORT;
+
+const app = express();
+
+if (!config.PRODUCTION) {
+    app.use(cors())
+}
+
+
+mongoose.connect(config.DATABASE_URL, (err) => {
+    if (err) {
+        console.log("error", err)
+    } else {
+        app.listen(port, () => {
+            console.log(`Server listenting on port ${port}`)
+        })
+    }
+})
+
